2277bdbe110430c983fc91dfcb297f058bd0f559
Miguel de Sousa <mfsousa@poli.ufrj.br>
2019-05-11 21:51:14 -0300

    port to play sine functions with mozzi lib

--- projeto_integrado/projeto_integrado.ino
+++ projeto_integrado/projeto_integrado.ino
@@ -1,19 +1,24 @@
+#include <MozziGuts.h>
+#include <Oscil.h>
+#include <tables/sin2048_int8.h>
+
 #include "pinout.h"
+
+#define CONTROL_RATE 128
 
 #define DEBUG                   1
 #define NUMBER_OF_BUTTONS       6
 #define KEY_ACTIVE              LOW
 
-// Set this to be the pin that your buzzer resides in. (Note that you can only have one buzzer actively using the PWM signal at a time).
-int tonePin = 9;
-int val = 0;   //variavel para ler status do pino
-
 int buttons_pins[NUMBER_OF_BUTTONS];
 int buttons_state[NUMBER_OF_BUTTONS];
-float state_frequencies[NUMBER_OF_BUTTONS];
 float frequency_multipliers[NUMBER_OF_BUTTONS];
-
+float frequency_values[NUMBER_OF_BUTTONS];
 float base_frequency = 880;
+
+Oscil<2048, AUDIO_RATE> osc[NUMBER_OF_BUTTONS];
+
+float outputSample = 0;
 
 void setup() {
   #if DEBUG
@@ -21,56 +26,36 @@ void setup() {
   #endif
   Serial.begin(9600);
   initialize();
+  startMozzi(CONTROL_RATE); 
 }
 
-
-void loop()
-{
+void updateControl(){
   updateButtons();
+}
 
-  int zeroButtonsCounter = 0;
-//
-//  if (buttons_state[0] == KEY_ACTIVE)
-//    tone(tonePin, base_frequency * frequency_multipliers[0], 1000);
-//  else
-//    noTone(tonePin);
+void loop(){
+    audioHook();
+}
 
+int updateAudio(){
+  outputSample = 0;
   for (int i = 0; i < NUMBER_OF_BUTTONS; i++) {
-
     if (buttons_state[i] == KEY_ACTIVE) {
-      tone(tonePin, base_frequency * frequency_multipliers[i]);
-      zeroButtonsCounter++;
+      outputSample += osc[i].next();
     }
   }
 
-  if (zeroButtonsCounter == 0)
-    noTone(tonePin);
+ #if DEBUG
+  Serial.print("Amplitude na saida:");
+  Serial.print(" ");
+  Serial.print((int) (outputSample)>>3);
+ #endif
   
+  return (int) (outputSample)>>3;
 }
-//  
-//  val = digitalRead(keyE);  // read input value
-//
-//  if (val == 0) {         //BOTAO APERTADO
-//    #if DEBUG
-//    Serial.println("KeyE pressed");
-//    #endif
-//    
-//    tone(tonePin, 329, 225.0);
-//    
-//     } else { // BOTAO SOLTO
-//      
-//        #if DEBUG
-//        Serial.println("KeyE not pressed");
-//        #endif
-//        
-//        noTone(tonePin);
-//     }
-//     delay(200);
 
 void updateButtons() {
-
   for (int i = 0; i < NUMBER_OF_BUTTONS; i++) {
-    
     if (digitalRead(buttons_pins[i]) == KEY_ACTIVE)
       buttons_state[i] = KEY_ACTIVE;
     else
@@ -86,7 +71,6 @@ void updateButtons() {
   }
   Serial.println();
   #endif
-
 }
 
 void initialize() {
@@ -104,7 +88,8 @@ void initialize() {
     buttons_state[i] = !KEY_ACTIVE;
 
     frequency_multipliers[i] = pow(2, i/12.0);
+    frequency_values[i] = base_frequency * frequency_multipliers[i];
+    osc[i].setTable(SIN2048_DATA);
+    osc[i].setFreq(frequency_values[i]); 
   }
-
-  pinMode(TONE_PIN, OUTPUT);
 }
