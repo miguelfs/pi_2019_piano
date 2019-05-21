481723c364a2ceb6e7c85679e1972d6a45fffc88
Miguel de Sousa <mfsousa@poli.ufrj.br>
2019-05-21 16:08:35 -0300

    testing 12 notes at labeca

--- projeto_integrado/pinout.h
+++ projeto_integrado/pinout.h
@@ -10,5 +10,11 @@
 #define KEY_3_PIN       6
 #define KEY_4_PIN       7
 #define KEY_5_PIN       8
+#define KEY_6_PIN       10
+#define KEY_7_PIN       11
+#define KEY_8_PIN       12
+#define KEY_9_PIN       13
+#define KEY_10_PIN      14
+#define KEY_11_PIN      15
 
 #endif
--- projeto_integrado/projeto_integrado.ino
+++ projeto_integrado/projeto_integrado.ino
@@ -8,7 +8,7 @@
 
 #define CONTROL_RATE            128
 #define DEBUG                   1
-#define NUMBER_OF_BUTTONS       6
+#define NUMBER_OF_BUTTONS       12
 #define KEY_ACTIVE              LOW
 
 int buttons_pins[NUMBER_OF_BUTTONS];
@@ -49,32 +49,59 @@ void loop(){
 // Bitwise operation used for fast division in order to decrease output saturation.
 int updateAudio(){
   return( (long) envelope[0].next() * osc[0].next()
-                + envelope[1].next() * osc[1].next()
-                + envelope[2].next() * osc[2].next() 
-                + envelope[3].next() * osc[3].next() 
-                + envelope[4].next() * osc[4].next() 
+ //               + envelope[1].next() * osc[1].next()
+ //               + envelope[2].next() * osc[2].next() 
+//                + envelope[3].next() * osc[3].next() 
+ //             + envelope[4].next() * osc[4].next() 
              // + envelope[5].next() * osc[5].next()
+                + envelope[6].next() * osc[6].next()
+                + envelope[7].next() * osc[7].next() 
+//                + envelope[8].next() * osc[8].next() 
+ //               + envelope[9].next() * osc[9].next()
+                + envelope[10].next() * osc[10].next() 
+    //            + envelope[11].next() * osc[11].next() 
                 ) >> 9;
 }
 
 void updateButtons() {
+
+  #if DEBUG
+  Serial.print("Estado dos botoes: ");
+  #endif
+  
   for (int i = 0; i < NUMBER_OF_BUTTONS; i++) {
     if (digitalRead(buttons_pins[i]) == KEY_ACTIVE) {
-      // 1 -> Button pressed, 0 otherwise
-      if (buttons_state[i] == KEY_ACTIVE) {
-       envelope[i].noteOn();
-      }        
+      
+      buttons_state[i] = !KEY_ACTIVE;
+  
     }
+    else
+      buttons_state[i] = KEY_ACTIVE;
+    #if DEBUG
+    Serial.print(" ");
+    Serial.print(buttons_state[i]);
+    #endif
+    
   }
+
+  #if DEBUG
+  Serial.println();
+  #endif
 }
 
 void initialize() {
-  buttons_pins[0] = KEY_0_PIN;
-  buttons_pins[1] = KEY_1_PIN;
-  buttons_pins[2] = KEY_2_PIN;
-  buttons_pins[3] = KEY_3_PIN;
-  buttons_pins[4] = KEY_4_PIN;
-  buttons_pins[5] = KEY_5_PIN;
+  buttons_pins[0]  = KEY_0_PIN;
+  buttons_pins[1]  = KEY_1_PIN;
+  buttons_pins[2]  = KEY_2_PIN;
+  buttons_pins[3]  = KEY_3_PIN;
+  buttons_pins[4]  = KEY_4_PIN;
+  buttons_pins[5]  = KEY_5_PIN;
+  buttons_pins[6]  = KEY_6_PIN;
+  buttons_pins[7]  = KEY_7_PIN;
+  buttons_pins[8]  = KEY_8_PIN;
+  buttons_pins[9]  = KEY_9_PIN;
+  buttons_pins[10] = KEY_10_PIN;
+  buttons_pins[11] = KEY_11_PIN;
   for (int i = 0; i < NUMBER_OF_BUTTONS; i++) {
      envelope[i].setTimes(70,200,400,300);
      envelope[i].setADLevels(255,128);
@@ -90,12 +117,18 @@ void initialize() {
     //frequency_multipliers[i] = pow(2, i/12.0);
     //frequency_values[i] = base_frequency * frequency_multipliers[i];
     
-    frequency_values[0] = 440;
-    frequency_values[1] = 495;
-    frequency_values[2] = 550;
-    frequency_values[3] = 586.67;
-    frequency_values[4] = 660;
-    frequency_values[5] = 733.33;
+    frequency_values[0] = 261.63;
+    frequency_values[1] = 272.54;
+    frequency_values[2] = 294.33;
+    frequency_values[3] = 313.96;
+    frequency_values[4] = 327.03;
+    frequency_values[5] = 348.83;
+    frequency_values[6] = 367.92;
+    frequency_values[7] = 392.44;
+    frequency_values[8] = 418.60;
+    frequency_values[9] = 436.05;
+    frequency_values[10] = 470.93;
+    frequency_values[11] = 490.55;
     
     osc[i].setTable(SIN2048_DATA);
     osc[i].setFreq(frequency_values[i]);
